af8a498bd724ce87e086815b05b2e915ecd76d73
Ilya Velo <bencubeincube@gmail.com>
2022-06-24 18:46:18 +0300

    added table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,54 @@
 import { Component, ViewChild } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NgForm } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-  myForm!: FormGroup
-  constructor(private FormBuilder: FormBuilder) {
-    this.myForm = this.FormBuilder.group({
-      name: [''],
-      age: ['22'],
-      job: ['']
-    })
-  }
+  itemsTitles = [
+    'Category',
+    'Items',
+    'Gl code',
+    'Amount',
+    'Sales tax',
+    'Total'
+  ]
 
-  submitForm() {
+  localItems = [
+    {
+      id: 1,
+      name: 'Category 1',
+      items: [{
+        id: 1,
+        name: 'asdddasd',
+        glcode: null,
+        amount: null,
+        salesTax: null,
+      }]
+    },
+    {
+      id: 2,
+      name: 'Category 2',
+      items: [{
+        id: 2,
+        name: 'asd54654ddasd',
+        glcode: null,
+        amount: null,
+        salesTax: null,
+      }]
+    }
+  ]
 
+  salesTaxArray = [
+    {id: 1, title: 'No tax', value: 0}, 
+    {id: 2, title: 'RU', value: 13},
+  ]
+
+  submitForm(myForm: NgForm) {
+    console.log(myForm)
   }
 }
